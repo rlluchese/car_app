65c4679841207ac7b5851f38047486445591d97a
Rafael Leites Luchese <rlluchese@gmail.com>
2023-02-13 15:24:22 -0300

    Car apps, using Redux
    Use of redux

---
+++ car_app/src/App.js
@@ -0,0 +1,25 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarValue from "./components/CarValue";
+import CarSearch from "./components/CarSearch";
+
+function App() {
+  return (
+    <div className="container is-fluid">
+      <CarForm>
+
+      </CarForm>
+      <CarSearch>
+        
+      </CarSearch>
+      <CarList>
+        
+      </CarList>
+      <CarValue>
+        
+      </CarValue>
+    </div>
+  );
+}
+
+export default App;
